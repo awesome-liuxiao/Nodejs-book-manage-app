3e15963d38d589caab3cd6b3c7a541067f983324
Liu, Xiao <lio4072@hotmail.com>
2017-05-18 22:06:23 +0900

    figured out how to receive post elements from form

--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,12 @@ var router = express.Router()
 
 router.post('/', function(req, res, next) {
     console.log('haha');
+    console.log(req.body.textID);
+    console.log(req.body.textPS);
     res.render('manbook', function(){
         console.log('rendered here.');
     })
+    res.redirect('/manbook')
 })
 
 module.exports = router
