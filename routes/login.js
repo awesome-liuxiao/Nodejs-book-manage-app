22cd339ee00f24f2eb735b05604b0bad8f3a968d
Liu, Xiao <lio4072@hotmail.com>
2017-05-18 22:13:13 +0900

    use two if() to verify the id and password of admin. until now, system only can check the admin's id and ps, also, the checking mechanism is very simple, just compare the id is wether 'admin', and ps is wether 'admin'

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,20 @@ var router = express.Router()
 
 router.post('/', function(req, res, next) {
     console.log('haha');
-    console.log(req.body.textID);
-    console.log(req.body.textPS);
+    console.log('id: '+req.body.textID);
+    console.log('ps: '+req.body.textPS);
+    var id = req.body.textID;
+    var ps = req.body.textPS;
+    if (id === 'admin') {
+        console.log('welcome, admin');
+        if (ps === 'admin') {
+            console.log('ps is correct');
+        } else {
+            console.log('ps is incorrect');
+        }
+    }else {
+        console.log(`invalid user, get out`);
+    }
     res.render('manbook', function(){
         console.log('rendered here.');
     })
